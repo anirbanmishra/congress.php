76f3b4478c04ee8be2ce03ebe36e6aba343b1bb4
anirbanmishra <anirbanmishra@ANIRBANs-MacBook-Pro.local>
2016-10-09 12:30:13 -0700

    Initial Comit

---
+++ congress.php
@@ -0,0 +1,29 @@
+<html xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html">
+<head>
+    <title>Congress Search Page</title>
+<body>
+<div id="form_div">
+    <form>
+        <label>Congress Database</label>
+        <select>
+            <option>Legislators</option>
+            <option>Committees</option>
+            <option>Bills</option>
+            <option>Amendments</option>
+        </select>
+        <br/>
+        <label>Chamber</label>
+        <input type="radio" value="senate" name="Senate"/>
+        <input type="radio" value="house" name="House">
+        <br/>
+        <label>Keyword</label>
+        <input type="text" name="search">
+        <br/>
+        <input type="button" name="Search" value="submit">
+        <input type="button" name="clear" value="clear">
+    </form>
+    <a href="http://sunlightfoundation.com/">Powered by Sunlight Foundation</a>
+</div>
+</body>
+</head>
+</html>
